728c96054a143f46bf3f7d85506e1a5377faf504
g00507732 <g00507732@notesmail.huawei.com>
2020-04-21 01:35:04 +0300

    TicketNo:AR000DT7BN Description:Connect platform datetime interfaces to Jerry Team:OTHERS Feature or Bugfix:Feature Binary Source:No PrivateCode(Yes/No):No
    Change-Id: I02abc8d5f047d3d2cc7bdfe9cfc63e4ea68e2496
    Reviewed-on: http://mgit-tm.rnd.huawei.com/8541672
    Reviewed-by: pengbiao 00240364 <pengbiao1@huawei.com>
    Tested-by: public jenkins <public_jenkins@notesmail.huawei.com>
    Reviewed-by: wangjiongfeng 00488524 <wangjiongfeng@huawei.com>

--- jerry-port/default/default-date.c
+++ jerry-port/default/default-date.c
@@ -31,6 +31,16 @@
 #include "jerryscript-port.h"
 #include "jerryscript-port-default.h"
 
+#ifdef JERRY_FOR_IAR_CONFIG
+#include "mc_type.h"
+#include "mc_hal_rtc.h"
+#define GET_SIGN_BIT(x) ((x) >> sizeof (x) * 8 - 1)
+#define GET_TIMEZONE_HOUR(timeZone) (((timeZone) >> 8) & 0x7F)
+#define GET_TIMEZONE_MIN(timeZone) ((timeZone) & 0xFF)
+#define HOUR_TO_MIN 60
+#define MIN_TO_SEC 60
+#endif
+
 #ifdef _WINDOWS
 /* https://support.microsoft.com/en-us/help/167296/how-to-convert-a-unix-time-t-to-a-win32-filetime-or-systemtime */
 void UnixTimeToFileTime (LONGLONG t, LPFILETIME pft)
@@ -66,6 +76,23 @@ double jerry_port_get_local_time_zone_adjustment (double unix_ms,  /**< ms since
 #else /* !HAVE_TM_GMTOFF */
   (void) unix_ms;
   (void) is_utc;
+#ifdef JERRY_FOR_IAR_CONFIG
+  // We don't use unix_ms and is_utc, since timezone cannot be computed
+  // as a function of given time.
+  MC_S16 time_zone = HAL_RtcGetTimeZone ();
+  // Time offset in seconds against GMT, absolute value.
+  MC_S32 time_offset = (GET_TIMEZONE_HOUR (time_zone) * HOUR_TO_MIN
+      + GET_TIMEZONE_MIN (time_zone)) * MIN_TO_SEC;
+  if (GET_SIGN_BIT(time_zone))
+  {
+    return -((double) time_offset) * 1000;
+  }
+  else
+  {
+    return ((double) time_offset) * 1000;
+  }
+
+#else
 #ifdef _WINDOWS
   FILETIME fileTime, localFileTime;
   SYSTEMTIME systemTime, localSystemTime;
@@ -85,6 +112,7 @@ double jerry_port_get_local_time_zone_adjustment (double unix_ms,  /**< ms since
     return ((LONGLONG) localTime.QuadPart - (LONGLONG) time.QuadPart) / 10000;
   }
 #endif /* _WINDOWS */
+#endif
   return 0.0;
 #endif /* HAVE_TM_GMTOFF */
 } /* jerry_port_get_local_time_zone_adjustment */
@@ -107,6 +135,15 @@ double jerry_port_get_current_time (void)
     return ((double) tv.tv_sec) * 1000.0 + ((double) tv.tv_usec) / 1000.0;
   }
 #endif /* __GNUC__ */
+
+#ifdef JERRY_FOR_IAR_CONFIG
+  MC_U64 millis = 0;
+  MC_S32 ret = HAL_RtcGetGmtTime (&millis, NULL);
+  if (ret == MC_SUCCESS)
+  {
+    return (double) millis;
+  }
+#endif
 
 #ifdef _WINDOWS
   time_t ltime;
